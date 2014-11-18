1ccd613ff0505ec367e7c93c406e3ce97bb8ed47
Robert Gravina <robert.gravina@kii.com>
2014-11-18 19:06:52 +0900

    CSS updates and mobile-like central view.

--- js/application.js
+++ js/application.js
@@ -1,11 +1,7 @@
 var ractive = new Ractive({
   el: 'dynamic',
   template: '#template',
-  data: { name: 'world' }
+  data: {text: 'this was updated dynamically'}
 });
 
 Kii.initializeWithSite("65d7aa9f", "783a838509d4699162e2181a8eb191c8", KiiSite.JP);
-
-// do something with the kii api
-
-ractive.set('name', 'world again')
