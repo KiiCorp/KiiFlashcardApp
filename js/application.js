9d1c7b3c728f43c5adbf4f06894377cf4c0fbc79
Robert Gravina <robert.gravina@kii.com>
2014-11-19 05:46:38 +0900

    Include route changes

--- js/application.js
+++ js/application.js
@@ -2,15 +2,6 @@ App = Ember.Application.create();
 
 App.Router.map(function() {
   this.route("signup", {path: "/signup" });
-  //this.route("signin", {path: "/signin" });
-});
-
-App.SignupRoute = Ember.Route.extend({
-  actions: {
-    createAccount: function() {
-      console.log("Creating account...");
-    }
-  }
 });
 
 Kii.initializeWithSite("65d7aa9f", "783a838509d4699162e2181a8eb191c8", KiiSite.JP);
