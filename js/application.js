279aa71da94be1c9f627dd1e392d7f7b130ca90c
Robert Gravina <robert.gravina@kii.com>
2014-11-18 18:26:28 +0900

    Added basic libraries css, and hello world.

---
+++ js/application.js
@@ -0,0 +1,11 @@
+var ractive = new Ractive({
+  el: 'container',
+  template: '#template',
+  data: { name: 'world' }
+});
+
+Kii.initializeWithSite("65d7aa9f", "783a838509d4699162e2181a8eb191c8", KiiSite.JP);
+
+// do something with the kii api
+
+ractive.set('name', 'world again')
