5797051891718aefd9a80faa499a31195e5dee0e
Robert Gravina <robert.gravina@kii.com>
2014-11-18 20:32:57 +0900

    Add Ember.

--- js/application.js
+++ js/application.js
@@ -1,7 +1,12 @@
-var ractive = new Ractive({
-  el: 'dynamic',
-  template: '#template',
-  data: {text: 'this was updated dynamically'}
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  this.route("signup", { path: "/signup" });
+  this.route("signin", { path: "/signin" });
+});
+
+App.IndexRoute = Ember.Route.extend({
+
 });
 
 Kii.initializeWithSite("65d7aa9f", "783a838509d4699162e2181a8eb191c8", KiiSite.JP);
