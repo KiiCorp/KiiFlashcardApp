0a13e8367e288ffd9a935b80720e3f10e5f30ff2
Robert Gravina <robert.gravina@kii.com>
2014-11-19 00:45:11 +0900

    Link action to create account button.

--- js/application.js
+++ js/application.js
@@ -5,8 +5,12 @@ App.Router.map(function() {
   //this.route("signin", {path: "/signin" });
 });
 
-// App.IndexRoute = Ember.Route.extend({
-
-// });
+App.SignupRoute = Ember.Route.extend({
+  actions: {
+    createAccount: function() {
+      console.log("Creating account...");
+    }
+  }
+});
 
 Kii.initializeWithSite("65d7aa9f", "783a838509d4699162e2181a8eb191c8", KiiSite.JP);
