e3b74e3413a6fc6fc0d10465c70975bb650418b9
Robert Gravina <robert.gravina@kii.com>
2014-11-18 20:37:53 +0900

    Index template.

--- js/application.js
+++ js/application.js
@@ -1,12 +1,12 @@
 App = Ember.Application.create();
 
-App.Router.map(function() {
-  this.route("signup", { path: "/signup" });
-  this.route("signin", { path: "/signin" });
-});
+// App.Router.map(function() {
+//   this.route("signup", { path: "/signup" });
+//   this.route("signin", { path: "/signin" });
+// });
 
-App.IndexRoute = Ember.Route.extend({
+// App.IndexRoute = Ember.Route.extend({
 
-});
+// });
 
 Kii.initializeWithSite("65d7aa9f", "783a838509d4699162e2181a8eb191c8", KiiSite.JP);
