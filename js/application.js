5dd2af2f0a293016911ab41c30e2e4535ef7fb09
Robert Gravina <robert.gravina@kii.com>
2014-11-19 21:15:35 +0900

    Show study words if there are words to study.

--- js/application.js
+++ js/application.js
@@ -5,6 +5,7 @@ App.Router.map(function() {
   this.route("signin", {path: "/signin" });
   this.route("home", {path: "/home" });
   this.route("getNewWords", {path: "/get-new-words" });
+  this.route("study", {path: "/study" });
 });
 
 Kii.initializeWithSite("65d7aa9f", "783a838509d4699162e2181a8eb191c8", KiiSite.JP);
