3d934e9305e0cb3deb9e0370a0c828b76031ed42
Robert Gravina <robert.gravina@kii.com>
2014-11-19 14:43:58 +0900

    Simple get new words message and button.

--- js/application.js
+++ js/application.js
@@ -4,6 +4,7 @@ App.Router.map(function() {
   this.route("signup", {path: "/signup" });
   this.route("signin", {path: "/signin" });
   this.route("home", {path: "/home" });
+  this.route("getNewWords", {path: "/get-new-words" });
 });
 
 Kii.initializeWithSite("65d7aa9f", "783a838509d4699162e2181a8eb191c8", KiiSite.JP);
